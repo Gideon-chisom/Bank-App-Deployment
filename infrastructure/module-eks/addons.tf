fc8bea24f1e0a75386c32ada314d7c94b7eb6ad6
Gideon-chisom <ogubuikegideon@gmail.com>
2026-05-12 15:23:30 +0100

    scaled up eks node group capacity

--- infrastructure/module-eks/addons.tf
+++ infrastructure/module-eks/addons.tf
@@ -1,27 +1,38 @@
 provider "helm" {
-    kubernetes {
-        host                   = aws_eks_cluster.eks.endpoint
-        cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-        token                  = data.aws_eks_cluster_auth.eks.token
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
 
-        # ADD THIS SAFETY DELAY PATTERN
-        exec {
-          api_version = "client.authentication.k8s.io/v1beta1"
-          command     = "aws"
-          args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
-        }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        aws_eks_cluster.eks.name
+      ]
     }
+  }
 }
 
 provider "kubernetes" {
   alias                  = "eks"
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
 
-data "aws_eks_cluster_auth" "eks" {
-    name = aws_eks_cluster.eks.name
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.eks.name
+    ]
+  }
 }
 
 resource "helm_release" "nginx_ingress" {
